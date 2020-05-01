5af55c3ea392f799b6085636afed0e18b1ebf37f
Richard Jean-Baptiste <Richinbk1@gmail.com>
2020-04-30 21:49:01 -0400

    SumSquareDifference

---
+++ SumSquareDifference.java
@@ -0,0 +1,27 @@
+class SumSquareDifference {
+
+    public static void main(String[] args) {
+        /*
+            The sum of the squares of the first ten natural numbers is,
+            1^2+2^2+...+10^2=385
+            The square of the sum of the first ten natural numbers is,
+            (1+2+...+10)^2=55^2=3025
+            Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 
+            3025−385=2640.
+            Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+        */
+
+        int range = 101;
+        
+        int x = 0;
+        int y = 0;
+
+        for(int i = 1; i < range ; i++){
+            x += (int) Math.pow(i, 2);
+            y += i;
+        }
+
+        System.out.println(((int) Math.pow(y, 2)) - (x));
+        
+    }    
+}
